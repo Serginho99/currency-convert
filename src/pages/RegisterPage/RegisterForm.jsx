ac3ad49a4c9bd58f150e80aa70677e3d959ee7b7
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-11-08 16:32:35 +0200

    update

--- src/pages/RegisterPage/RegisterForm.jsx
+++ src/pages/RegisterPage/RegisterForm.jsx
@@ -1,4 +1,5 @@
 // import { useState } from 'react';
+
 import { useDispatch } from 'react-redux';
 import authOperations from 'redux/auth/authOperations';
 import * as React from 'react';
@@ -21,24 +22,52 @@ import Alert from '@mui/material/Alert';
 const theme = createTheme();
 
 export default function RegisterForm() {
+  // const [error, setError] = useState(null);
+  // const [name, setName] = useState(null);
+  // const [isUser, setIsUser] = useState(null);
+  const [name, setName] = useState('');
+  const [nameError, setNameError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
   const [error, setError] = useState(null);
-  const [name, setName] = useState(null);
-  const [isUser, setIsUser] = useState(null);
-  const [passwordError, setPasswordError] = useState(null);
+  // const [passwordError, setPasswordError] = useState(null);
+
+  const options = { name: setName, email: setEmail, password: setPassword };
+
+  function onChange({ target: { name, value } }) {
+    options[name](value.toLowerCase());
+  }
+  // console.log(name);
 
   const dispatch = useDispatch();
 
   const handleSubmit = async event => {
     event.preventDefault();
-    // if (user === undefined) {
-    //   return;
-    // }
+
+    if (name === '') {
+      setNameError(true);
+      return;
+    } else {
+      setNameError(false);
+    }
+
+    if (email === '') {
+      setEmailError(true);
+      return;
+    } else {
+      setEmailError(false);
+    }
+
+    if (password.length < 7) {
+      setPasswordError(true);
+      return;
+    } else {
+      setPasswordError(false);
+    }
+
     const data = new FormData(event.currentTarget);
-    // console.log({
-    //   name: data.get('name'),
-    //   email: data.get('email'),
-    //   password: data.get('password'),
-    // });
     const res = await dispatch(
       authOperations.register({
         name: data.get('name'),
@@ -46,192 +75,189 @@ export default function RegisterForm() {
         password: data.get('password'),
       })
     );
-    console.log(res.payload);
-    const nameUser = event.target.elements.name.value;
-    const passwordLength = event.target.elements.password.value.length;
-    const emailLength = event.target.elements.email.value;
-    if (nameUser === '') {
-      setName(true);
-    } else {
-      setName(null);
+    console.log(res);
+    if (res.payload.status === 400) {
+      setError('Wrong password or email');
+      return;
     }
-    if (passwordLength < 7) {
-      setPasswordError(true);
-    } else {
-      setPasswordError(null);
-    }
-    if (res.payload.message === undefined) {
-      setIsUser('User with this email already exists');
-    } else {
-      setIsUser(null);
-    }
-    if (
-      res.payload.message === 'User validation failed: email: Invalid email.' ||
-      emailLength === ''
-    ) {
-      setError(true);
-    } else {
-      setError(null);
-    }
+
+    // const nameUser = event.target.elements.name.value;
+    // const passwordLength = event.target.elements.password.value.length;
+    // const emailLength = event.target.elements.email.value;
+
+    // if (nameUser === '') {
+    //   setName(true);
+    // } else {
+    //   setName(null);
+    // }
+
+    // if (passwordLength < 7) {
+    //   setPasswordError(true);
+    // } else {
+    //   setPasswordError(null);
+    // }
+
+    // if (res.payload.message === undefined) {
+    //   setIsUser('User with this email already exists');
+    // } else {
+    //   setIsUser(null);
+    // }
+
+    // if (
+    //   res.payload.message === 'User validation failed: email: Invalid email.' ||
+    //   emailLength === ''
+    // ) {
+    //   setError(true);
+    // } else {
+    //   setError(null);
+    // }
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
+    <>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                {!name ? (
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  {error && (
+                    <Alert
+                      style={{ marginBottom: '15px' }}
+                      variant="outlined"
+                      severity="error"
+                    >
+                      {error}
+                    </Alert>
+                  )}
                   <TextField
+                    onChange={onChange}
+                    error={nameError}
                     autoComplete="given-name"
                     name="name"
+                    // value={name}
                     required
                     fullWidth
                     id="name"
                     label="Name"
                     autoFocus
                   />
-                ) : (
+                </Grid>
+                <Grid item xs={12}>
+                  {/* {isUser && (
+                    <Alert
+                      variant="outlined"
+                      severity="error"
+                      style={{ marginBottom: '15px' }}
+                    >
+                      {isUser}
+                    </Alert>
+                  )} */}
                   <TextField
-                    error
-                    autoComplete="given-name"
-                    name="name"
+                    onChange={onChange}
                     required
                     fullWidth
-                    id="name"
-                    label="Invalid name"
-                    autoFocus
-                  />
-                )}
-              </Grid>
-              {/* <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid> */}
-              <Grid item xs={12}>
-                {isUser && (
-                  <Alert
-                    variant="outlined"
-                    severity="error"
-                    style={{ marginBottom: '15px' }}
-                  >
-                    {isUser}
-                  </Alert>
-                )}
-                {!error ? (
-                  <TextField
-                    required
-                    fullWidth
+                    error={emailError}
                     id="email"
-                    label="Email Address"
+                    label="Email"
                     name="email"
+                    // value={email}
                     autoComplete="email"
+                    type="email"
                   />
-                ) : (
-                  <TextField
-                    required
-                    fullWidth
-                    error
-                    id="email"
-                    label="Invalid email"
-                    name="email"
-                    autoComplete="email"
-                  />
-                )}
-              </Grid>
+                </Grid>
 
-              <Grid item xs={12}>
-                {!passwordError ? (
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                ) : (
-                  <TextField
-                    required
-                    fullWidth
-                    error
-                    name="password"
-                    label="Password is shorter than the minimum allowed length (7)"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                )}
+                <Grid item xs={12}>
+                  {!passwordError ? (
+                    <TextField
+                      onChange={onChange}
+                      required
+                      fullWidth
+                      error={passwordError}
+                      name="password"
+                      // value={password}
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                    />
+                  ) : (
+                    <TextField
+                      onChange={onChange}
+                      required
+                      fullWidth
+                      error={passwordError}
+                      name="password"
+                      // value={password}
+                      label="Password is shorter than the minimum allowed length (7)."
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                    />
+                  )}
+                </Grid>
+                {/* <Grid item xs={12}>
+          <FormControlLabel
+            control={
+              <Checkbox value="allowExtraEmails" color="primary" />
+            }
+            label="I want to receive inspiration, marketing promotions and updates via email."
+          />
+        </Grid> */}
               </Grid>
-              {/* <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid> */}
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="login" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
 
 // export default function RegisterForm() {
-//   const [name, setName] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
+// const [name, setName] = useState('');
+// const [email, setEmail] = useState('');
+// const [password, setPassword] = useState('');
 //   const dispatch = useDispatch();
 
-//   const options = { name: setName, email: setEmail, password: setPassword };
+// const options = { name: setName, email: setEmail, password: setPassword };
 
-//   function onChange({ target: { name, value } }) {
-//     options[name](value);
-//   }
+// function onChange({ target: { name, value } }) {
+//   options[name](value);
+// }
 
 //   function onSubmit(e) {
 //     e.preventDefault();
