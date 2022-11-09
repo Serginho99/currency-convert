edf399e779e7d2188a4bb9e26440ef3bf1836e1f
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-11-09 12:18:14 +0200

    Update NavAuth.styled.jsx

--- src/components/NavBar/NavAuth.styled.jsx
+++ src/components/NavBar/NavAuth.styled.jsx
@@ -5,16 +5,15 @@ export const NavAuthSignUp = styled(NavLink)`
   color: white;
   font-size: 30px;
   transition: all 250ms;
-  /* padding: 10px; */
+  padding: 10px;
   border-radius: 5px;
-  /* position: relative; */
 
   &.active {
     color: white;
-    background-color: #5e5eef;
+    background-color: #3378af;
   }
   &:hover:not(.active) {
-    color: #9696e9;
+    color: #3378af;
   }
 `;
 
@@ -22,16 +21,16 @@ export const NavAuthSignIn = styled(NavLink)`
   color: white;
   font-size: 30px;
   transition: all 250ms;
-  /* padding: 10px; */
+  padding: 10px;
   border-radius: 5px;
   margin-left: 22px;
 
   &.active {
     color: white;
-    background-color: #5e5eef;
+    background-color: #3378af;
   }
   &:hover:not(.active) {
-    color: #9696e9;
+    color: #3378af;
   }
 `;
 
@@ -46,6 +45,6 @@ export const Box = styled.div`
     top: 50%;
     left: 52%;
     transform: translate(-50%, -50%);
-    background-color: black;
+    background-color: #1a237e;
   }
 `;
