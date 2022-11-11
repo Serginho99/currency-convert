02f824dc90b193c4d40d7884a9657796255f2e00
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-11-11 16:48:30 +0200

    Update ContactItem.jsx

--- src/components/contactItem/ContactItem.jsx
+++ src/components/contactItem/ContactItem.jsx
@@ -15,26 +15,6 @@ import ChangeContact from 'components/ChangeContact/ChangeContact';
 import { InfinitySpin } from 'react-loader-spinner';
 import BasicAvatar from 'components/BasicAvatar/BasicAvatar';
 
-function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
 function ContactItem({ name, number, id }) {
   const [deleteContact, { isLoading }] = useDeleteContactMutation();
   const { isOpen, setIsOpen, toggle } = useToggle();
